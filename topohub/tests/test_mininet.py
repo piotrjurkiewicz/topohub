f27215e399bc9a39b8b520937eebd3cc6cc48a12
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2023-08-02 03:02:23 +0200

    Add mininet test

---
+++ topohub/tests/test_mininet.py
@@ -0,0 +1,12 @@
+import mininet.net
+from topohub.mininet import TOPO
+
+def test_mininet():
+    for name in ['gabriel/25/0', 'sndlib/germany50']:
+        topo_cls = TOPO[name]
+        topo = topo_cls()
+        print(topo)
+
+
+if __name__ == '__main__':
+    test_mininet()
