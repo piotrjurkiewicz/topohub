0880617d645dac66694b9fe6eb167ab8acb441af
Piotr Jurkiewicz <piotr.jerzy.jurkiewicz@gmail.com>
2024-06-22 19:20:06 +0200

    Update Mininet test

--- topohub/tests/test_mininet.py
+++ topohub/tests/test_mininet.py
@@ -1,8 +1,8 @@
-from topohub.mininet import TOPO
+from topohub.mininet import TOPO_CLS
 
 def test_mininet():
     for name in ['gabriel/25/0', 'sndlib/germany50']:
-        topo_cls = TOPO[name]
+        topo_cls = TOPO_CLS[name]
         topo = topo_cls()
         print(topo)
 
